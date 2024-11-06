cd19ba3f9526134804e519e86f57812a196b3331
Le Hieu <hieultph49402@gmail.com>
2024-11-06 21:14:44 +0700

    du an

--- admin/controllers/danhmucController.php
+++ admin/controllers/danhmucController.php
@@ -1,5 +1,58 @@
 <?php
-    class controller{
-        
+    class danhMucController{
+        public $danhMucModel;
+        function __construct()
+        {
+            $this->danhMucModel = new danhMucModel;
+        }
+
+        function hien_thi_danh_muc(){
+            $danh_muc = $this->danhMucModel->danh_muc();
+            require_once 'views/danhMucView/danhMuc.html';
+        }
+
+        function sua_danh_muc($id){
+            $gia_tri = $this->danhMucModel->tim_danh_muc($id);
+            require_once "views/danhMucView/suaDanhMuc.html";
+            if( isset($_POST['btn_sua']) ){
+                $id = $_POST['id'];
+                $ten_danh_muc = $_POST['ten_danh_muc'];
+                $hinh_anh = $POST['hinh_anh'];
+                $ngay_tao = date("Y-m-d");
+                $ngay_cap_nhat = date("Y-m-d");
+                $mo_ta = $POST['mo_ta'];
+                if($this->danhMucModel->sua_danh_muc($id,$ten_danh_muc,$hinh_anh,$ngay_tao, $ngay_cap_nhat, $mo_ta)){
+                    header('Location:?act=trangChuAdmin');
+                    exit();
+                }else{
+                    echo "Khong the sua";
+                }
+            }
+        }
+
+        function them_danh_muc(){
+            require_once 'views/themDanhMuc.html';
+            if(isset($_POST['btn_them'])){
+                $ten_danh_muc = $_POST['ten_danh_muc'];
+                $hinh_anh = $POST['hinh_anh'];
+                $ngay_tao = date("Y-m-d");
+                $ngay_cap_nhat = date("Y-m-d");
+                $mo_ta = $POST['mo_ta'];
+                if($this->danhMucModel->them_danh_muc($ten_danh_muc,$hinh_anh,$ngay_tao, $ngay_cap_nhat, $mo_ta)){
+                    header('Location:?act=trangChuAdmin');
+                    exit();
+                }else{
+                    echo "Khong the them danh muc";
+                }
+            }
+        }
+
+        function xoa_danh_muc($id){
+            if($this->danhMucModel->xoa_danh_muc($id)){
+                header('Location:?act=trangChuAdmin');
+            }else{
+                echo "Khong the xoa danh muc";
+            }
+        }
     }
 ?>
--- admin/models/danhmucModel.php
+++ admin/models/danhmucModel.php
@@ -1,3 +1,40 @@
 <?php
+    class danhMucModel{
+        public $conn;
 
+        function __construct()
+        {
+            $this->conn = connDB();
+        }
+
+        function danh_muc(){
+            $sql = "select * from danh_mucs";
+            return $this->conn->query($sql);
+        }
+
+        function them_danh_muc($ten_danh_muc, $hinh_anh , $mo_ta){
+            $sql = "insert into danh_mucs value(null, '$ten_danh_muc' , '$hinh_anh' , null , null , '$mo_ta')";
+            $stnt = $this->conn->prepare($sql);
+            return $stnt->execute();
+        }
+
+        function sua_danh_muc($id,$ten_danh_muc,$hinh_anh,$ngay_tao, $ngay_cap_nhat, $mo_ta){
+            $sql = "update danh_mucs set id=$id , ten_danh_muc = '$ten_danh_muc', hinh_anh = '$hinh_anh' mo_ta = '$mo_ta";
+            $stnt = $this->conn->prepare($sql);
+            return $stnt->execute();
+        }
+
+        function xoa_danh_muc($id){
+            $sql = "delete from danh_mucs where id = $id";
+            $stnt = $this->conn->prepare($sql);
+            return $stnt->execute();
+        }
+
+        function tim_danh_muc($id){
+            $sql = "select * from danh_mucs where id = $id";
+            return $this->conn->query($sql);
+        }
+
+    
+    }
 ?>
